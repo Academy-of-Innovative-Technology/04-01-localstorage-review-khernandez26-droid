7008d558fe21622ac899d2e52a3c76179bd4c9bc
Keilah <Khernandez26@aoiths.org>
2026-02-03 13:44:13 -0500

    working

--- script.js
+++ script.js
@@ -3,21 +3,24 @@ var key = "";
 
 // SAVE
 document.querySelector(".save").onclick = function() {
+localStorage.setItem( key, value )
+	const save = localStorage.getItem(key, value);
 
-};
-
-
-// LOAD
-document.querySelector(".load").onclick = function() {
-
+    console.log( save )
 };
 
 // LOAD
 document.querySelector(".update").onclick = function() {
+    const load = localStorage.getItem(key);
+    console.log( load )
 
+    localStorage.setItem(key, load);
 };
 
 // DELETE
 document.querySelector(".delete").onclick = function() {
+    localStorage.removeItem( key )
+    localStorage.clear()
 
+    console.log( ".delete" )
 };
