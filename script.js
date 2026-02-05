e5743dde8fe0f0871c14558de55a006ee78d6f96
Keilah <Khernandez26@aoiths.org>
2026-02-05 14:04:58 -0500

    Completed.

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-var key = "";
+
+var key = "savedName";
+
+const dataInput = document.querySelector(".data-input");
+const deleteInput = document.querySelector(".delete-input");
+
 
 
 // SAVE
 document.querySelector(".save").onclick = function() {
-localStorage.setItem( key, value )
-	const save = localStorage.getItem(key, value);
 
-    console.log( save )
+	const value = dataInput.value;
+
+ localStorage.setItem( key, value );
+
+ dataInput.value = "";
+
+    console.log("Saved", value )
 };
 
 // LOAD
 document.querySelector(".update").onclick = function() {
-    const load = localStorage.getItem(key);
-    console.log( load )
 
-    localStorage.setItem(key, load);
+    const load = localStorage.getItem(key);
+    
+    dataInput.value = load;
+
+    console.log( "Loaded", load )
 };
 
 // DELETE
 document.querySelector(".delete").onclick = function() {
-    localStorage.removeItem( key )
-    localStorage.clear()
 
-    console.log( ".delete" )
+   const deleteKey = deleteInput.value;
+
+   localStorage.removeItem(deleteKey);
+
+   console.log("DeletedKey:", deleteKey);
 };
